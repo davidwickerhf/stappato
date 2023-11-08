6348dc9b756feca03946d25de77a8f2f0dad93e8
David Wicker <davidwickerhf@gmail.com>
2023-11-08 13:41:30 +0100

    Initialize Tailwindcss and add start assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,31 @@
 <script>
+	import '../app.css';
 	import './styles.css';
 </script>
 
+<!-- Import material icons -->
+<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+/>
+
 <div class="app">
+	<!-- Navbar -->
+	<!-- To each page add xxx margin to the top (including image overflow) -->
+	<div class="px-6 py-6">
+		<span class="material-symbols-outlined md:invisible"> menu </span>
+	</div>
+
 	<main>
 		<slot />
 	</main>
 
+	<!-- Footer -->
 	<footer />
 </div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.background.three);
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: {
+					three: '#FEFCF2'
+				},
+				stappatored: {
+					three: '#B65139'
+				},
+				text: {
+					three: '#000000'
+				},
+				darkbrown: {
+					three: '#5D4236'
+				},
+				lightbrown: {
+					three: '#764F3E'
+				}
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/container-queries')]
+};
