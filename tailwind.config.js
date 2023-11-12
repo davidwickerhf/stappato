36be2077fc448b8226708175befcddc4bee184cb
David Wicker <davidwickerhf@gmail.com>
2023-11-12 19:28:40 +0100

    No JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	mode: 'jit',
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
