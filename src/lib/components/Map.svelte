68f952dd3b79604075ebd0a928e3854cdee0cec9
David Wicker <davidwickerhf@gmail.com>
2023-11-15 14:22:56 +0100

    Extract Map component from +page.svelte

---
+++ src/lib/components/Map.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { theme } from '$lib/constants/theme';
+</script>
+
+<div
+	class="flex lg:row-span-2 w-full max-h-[275px] lg:max-h-[375px] lg:max-w-[550px] transition-all duration-200 ease-in-out"
+>
+	<a
+		href="https://maps.app.goo.gl/Wv3ZNrFMotgb3Qq68"
+		class="w-full h-full transition-all duration-200 ease-in-out map hover:cursor-pointer lg:bg-[auto_100%] bg-[100%_auto] hover:bg-[120%_auto] lg:hover:bg-[auto_120%]"
+		style="--white: {theme.colors.background.three};"
+	>
+		<img
+			class="invisible w-full h-full"
+			alt="map hidden "
+			src="/images/assets/stappatomap.png"
+			style="visibility: hidden;"
+		/>
+	</a>
+</div>
+
+<style lang="scss">
+	.map {
+		z-index: 10;
+		background-color: var(--white);
+		background-image: url('/images/assets/stappatomap.png');
+		mix-blend-mode: darken;
+		background-position: center center;
+		background-repeat: no-repeat;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Map from '../lib/components/Map.svelte';
+
 	import Logo from '../lib/components/Logo.svelte';
 
 	import Button from '$lib/components/Button.svelte';
@@ -66,22 +68,7 @@
 		</div>
 
 		<!-- Map image -->
-		<div
-			class="flex lg:row-span-2 w-full max-h-[275px] lg:max-h-[375px] lg:max-w-[550px] transition-all duration-200 ease-in-out"
-		>
-			<a
-				href="https://maps.app.goo.gl/Wv3ZNrFMotgb3Qq68"
-				class="w-full h-full transition-all duration-200 ease-in-out map hover:cursor-pointer lg:bg-[auto_100%] bg-[100%_auto] hover:bg-[120%_auto] lg:hover:bg-[auto_120%]"
-				style="--white: {theme.colors.background.three};"
-			>
-				<img
-					class="invisible w-full h-full"
-					alt="map hidden "
-					src="/images/assets/stappatomap.png"
-					style="visibility: hidden;"
-				/>
-			</a>
-		</div>
+		<Map />
 
 		<!-- Action buttons -->
 		<div
@@ -143,14 +130,3 @@
 	<div />
 </div>
 <div class="h-screen">3</div>
-
-<style lang="scss">
-	.map {
-		z-index: 10;
-		background-color: var(--white);
-		background-image: url('/images/assets/stappatomap.png');
-		mix-blend-mode: darken;
-		background-position: center center;
-		background-repeat: no-repeat;
-	}
-</style>
