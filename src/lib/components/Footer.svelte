7624cf5adebd4cbb3d1058385c792785e01587fe
David Wicker <davidwickerhf@gmail.com>
2023-11-17 17:29:43 +0100

    Work on footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,81 +1,94 @@
 <script lang="ts">
-	import { padding, paddingY } from '$lib/constants/theme';
+	import { padding, paddingRight, paddingTop, paddingY } from '$lib/constants/theme';
 	import { mapUrl, reserveUrl } from '$lib/constants/urls';
 </script>
 
 <!-- Footer -->
-<footer class="flex flex-col w-screen gap-10 md:gap-20 bg-stappatored-three text-background-three">
+<footer class="flex flex-col w-screen bg-stappatored-three text-background-three">
 	<!--  Contents -->
-	<div
-		class="flex flex-col sm:flex-row sm:flex-wrap w-screen {padding} {paddingY} justify-center sm:justify-start gap-10 basis-0"
-	>
-		<!-- Items -->
-		<!-- Pages -->
-		<div class="flex flex-col gap-2 tracking-[1px] flex-grow-0">
-			<div class="flex items-center gap-2 font-medium uppercase">
-				<span class="material-symbols-outlined" style="font-size: 20px"> home </span>
-				Pages
-			</div>
-			<a href="/" class="flex-grow-0 font-thin hover:underline max-w-fit">About</a>
-			<a href="/menu" class="flex-grow-0 font-thin hover:underline max-w-fit">Menu</a>
-			<a href="/team" class="flex-grow-0 font-thin hover:underline max-w-fit">Team</a>
-			<a href="/contact" class="flex-grow-0 font-thin hover:underline max-w-fit">Contact</a>
-		</div>
+	<div class="flex items-start">
+		<div
+			class="flex flex-col sm:flex-row sm:flex-wrap lg:grid-cols-4 w-screen {padding} {paddingY} justify-center sm:justify-start gap-10 md:gap-20 items-start"
+		>
+			<!-- Items -->
 
-		<!-- Opening times -->
-		<div class="flex flex-col gap-2 tracking-[1px]">
-			<div class="flex items-center gap-2 font-medium uppercase">
-				<span class="material-symbols-outlined" style="font-size: 20px"> watch </span>
-				Opening times
+			<!-- Opening times -->
+			<div class="flex flex-col gap-2 tracking-[1px]">
+				<div class="flex items-center gap-2 font-medium uppercase">
+					<span class="material-symbols-outlined" style="font-size: 20px"> watch </span>
+					Opening times
+				</div>
+				<p class="font-thin">Monday</p>
+				<p class="font-thin">Tuesday</p>
+				<p class="font-thin">Wednesday</p>
+				<p class="font-thin">Thursday</p>
+				<p class="font-thin">Friday</p>
+				<p class="font-thin">Saturday</p>
+				<p class="font-thin">Sunday</p>
 			</div>
-			<p class="font-thin">Monday</p>
-			<p class="font-thin">Tuesday</p>
-			<p class="font-thin">Wednesday</p>
-			<p class="font-thin">Thursday</p>
-			<p class="font-thin">Friday</p>
-			<p class="font-thin">Saturday</p>
-			<p class="font-thin">Sunday</p>
-		</div>
 
-		<!-- Address -->
-		<div class="flex flex-col gap-2 tracking-[1px]">
-			<div class="flex items-center gap-2 font-medium uppercase">
-				<span class="material-symbols-outlined" style="font-size: 20px"> map </span>
-				Address
+			<!-- Address -->
+			<div class="flex flex-col gap-2 tracking-[1px]">
+				<div class="flex items-center gap-2 font-medium uppercase">
+					<span class="material-symbols-outlined" style="font-size: 20px"> map </span>
+					Address
+				</div>
+				<p class="font-thin">Markt 36</p>
+				<p class="font-thin">6211 CK Maastricht</p>
+				<p class="font-thin">Netherlands</p>
+				<a href={mapUrl} class="flex items-center gap-4 font-medium max-w-fit"
+					><span class="hover:underline">View on maps </span><span class="material-symbols-outlined"
+						>arrow_right_alt</span
+					></a
+				>
 			</div>
-			<p class="font-thin">Markt 36</p>
-			<p class="font-thin">6211 CK Maastricht</p>
-			<p class="font-thin">Netherlands</p>
-			<a href={mapUrl} class="flex items-center gap-4 font-medium max-w-fit"
-				><span class="hover:underline">View on maps </span><span class="material-symbols-outlined"
-					>arrow_right_alt</span
-				></a
-			>
-		</div>
 
-		<!-- Get in touch -->
-		<div class="flex flex-col gap-2 tracking-[1px]">
-			<div class="flex items-center gap-2 font-medium uppercase">
-				<span class="material-symbols-outlined" style="font-size: 20px"> phone </span>
-				Get in touch
+			<!-- Pages -->
+			<div class="flex flex-col gap-2 tracking-[1px]">
+				<div class="flex items-center gap-2 font-medium uppercase">
+					<span class="material-symbols-outlined" style="font-size: 20px"> home </span>
+					Pages
+				</div>
+				<a href="/" class="flex-grow-0 font-thin hover:underline max-w-fit">About</a>
+				<a href="/menu" class="flex-grow-0 font-thin hover:underline max-w-fit">Menu</a>
+				<a href="/team" class="flex-grow-0 font-thin hover:underline max-w-fit">Team</a>
+				<a href="/contact" class="flex-grow-0 font-thin hover:underline max-w-fit">Contact</a>
 			</div>
-			<a href={reserveUrl} class="flex items-center gap-4 font-medium max-w-fit"
-				><span class="hover:underline">Reserve</span><span class="material-symbols-outlined"
-					>arrow_right_alt</span
-				></a
-			>
-			<a href="/contact" class="flex items-center gap-4 font-medium max-w-fit"
-				><span class="hover:underline">Contact us </span><span class="material-symbols-outlined"
-					>arrow_right_alt</span
-				></a
-			>
-			<a href={mapUrl} class="flex items-center gap-4 font-medium max-w-fit"
-				><span class="hover:underline">View on maps </span><span class="material-symbols-outlined"
-					>arrow_right_alt</span
-				></a
-			>
+
+			<!-- Get in touch -->
+			<div class="flex flex-col gap-2 tracking-[1px]">
+				<div class="flex items-center gap-2 font-medium uppercase">
+					<span class="material-symbols-outlined" style="font-size: 20px"> phone </span>
+					Get in touch
+				</div>
+				<a href={reserveUrl} class="flex items-center gap-4 font-medium max-w-fit"
+					><span class="hover:underline">Reserve</span><span class="material-symbols-outlined"
+						>arrow_right_alt</span
+					></a
+				>
+				<a href="/contact" class="flex items-center gap-4 font-medium max-w-fit"
+					><span class="hover:underline">Contact us </span><span class="material-symbols-outlined"
+						>arrow_right_alt</span
+					></a
+				>
+				<a href={mapUrl} class="flex items-center gap-4 font-medium max-w-fit"
+					><span class="hover:underline">View on maps </span><span class="material-symbols-outlined"
+						>arrow_right_alt</span
+					></a
+				>
+			</div>
+		</div>
+		<div
+			class="hidden {paddingTop} md:flex md:max-w-[25%] lg:max-w-[15%] max-h-full {paddingRight} items-end h-full"
+		>
+			<img
+				alt="Diving cork"
+				src="/images/corkscrews/Diving Cork.png"
+				class="bg-stappatored-three mix-blend-darken"
+			/>
 		</div>
 	</div>
+
 	<!-- Bottom bar -->
 	<div class="flex w-screen border-t-[1px] border-background-three h-20 align-center items-center">
 		<!-- Logo -->
@@ -85,7 +98,9 @@
 			Stappato
 		</p>
 		<!-- Copyright -->
-		<a href="https://david.wicker.life/" class="{padding} font-thin text-sm hover:underline"
+		<a
+			href="https://david.wicker.life/"
+			class="{padding} font-thin text-sm hover:underline md:text-base"
 			>© 2023 David Henry Francis Wicker. All rights reserved.</a
 		>
 	</div>
--- src/lib/constants/theme.ts
+++ src/lib/constants/theme.ts
@@ -18,6 +18,11 @@ export const theme = {
 	}
 };
 
-export const padding = 'px-6 sm:px-8 md:px-10 lg:px-16';
-export const paddingY = 'py-10 sm:py-12 md:py-20';
+export const paddingRight = ' pr-6 sm:pr-8 md:pr-10 lg:pr-16 ';
+export const paddingLeft = ' pl-6 sm:pl-8 md:pl-10 lg:pl-16 ';
+export const padding = paddingLeft + paddingRight;
+
+export const paddingTop = ' pt-10 sm:pt-12 md:pt-20 ';
+export const paddingBottom = ' pb-10 sm:pb-12 md:pb-20 ';
+export const paddingY = paddingTop + paddingBottom;
 export const h2 = ' sm:text-2xl md:text-3xl';
