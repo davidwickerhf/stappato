e9131cc57681dff69032969e4e9cb4da6c46e52b
David Wicker <davidwickerhf@gmail.com>
2023-11-17 19:26:32 +0100

    Implement Team Section

---
+++ src/lib/components/Employee.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let name: string;
+	export let position: string;
+	export let bio: string;
+	export let src: string;
+</script>
+
+<!-- Individual grid item -->
+<div class="w-[295px] h-[445px] shadow-2xl bg-background-three">
+	<!-- Profile picture -->
+	<div class="relative overflow-hidden h-[195px]">
+		<img class="absolute top-0 w-full" alt="staff profile" {src} />
+	</div>
+
+	<!-- Content -->
+	<div class="flex flex-col gap-4 px-6 py-6">
+		<!-- Name and title -->
+		<div class="flex flex-col gap-[2px]">
+			<p class="font-medium tracking-[1px]">{name}</p>
+			<p class="font-thin tracking-[1px]">{position}</p>
+		</div>
+
+		<!-- Description -->
+		<p>
+			{bio}
+		</p>
+	</div>
+</div>
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+	import Employee from '../../lib/components/Employee.svelte';
+
 	import LandingPicture from '$lib/components/LandingPicture.svelte';
+	import TextButton from '$lib/components/TextButton.svelte';
+	import { h2, padding, paddingY } from '$lib/constants/theme';
 </script>
 
 <svelte:head>
@@ -9,3 +13,69 @@
 
 <!-- Landing picture -->
 <LandingPicture title="Our team" />
+
+<!-- Special requests -->
+<section
+	class="relative {padding} {paddingY} grid gap-8 grid-cols-[auto_auto] grid-rows-[auto] justify-between align-middle items-center mb-20 md:mb-30 lg:mb-40"
+>
+	<div class="flex flex-col w-full min-h-fit">
+		<h2 class="text-darkbrown-three {h2}">Meet the stappato team</h2>
+
+		<!-- GRID -->
+		<div class="flex flex-row flex-wrap gap-6 py-10">
+			<Employee
+				bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua."
+				name="Tim de Vrouw"
+				position="Founder and Chef"
+				src="/images/images/tim.jpg"
+			/>
+			<Employee
+				bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua."
+				name="Tim de Vrouw"
+				position="Founder and Chef"
+				src="/images/images/tim.jpg"
+			/>
+			<Employee
+				bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua."
+				name="Tim de Vrouw"
+				position="Founder and Chef"
+				src="/images/images/tim.jpg"
+			/>
+			<Employee
+				bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua."
+				name="Tim de Vrouw"
+				position="Founder and Chef"
+				src="/images/images/tim.jpg"
+			/>
+			<Employee
+				bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua."
+				name="Tim de Vrouw"
+				position="Founder and Chef"
+				src="/images/images/tim.jpg"
+			/>
+			<Employee
+				bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua."
+				name="Tim de Vrouw"
+				position="Founder and Chef"
+				src="/images/images/tim.jpg"
+			/>
+		</div>
+
+		<!-- Link -->
+		<TextButton url="/contact">Want to join the team? Reach out!</TextButton>
+	</div>
+
+	<div class="absolute right-20 -z-10 opacity-20 -bottom-2 md:bottom-6 lg:bottom-4 lg:right-40">
+		<img
+			alt="Dancing Corks"
+			src="/images/corkscrews/Dancing Corks.png"
+			class="max-h-[435px] md:max-h-[525px] lg:max-h-[625px]"
+		/>
+	</div>
+</section>
