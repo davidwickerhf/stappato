141de8398a8c89ff16e19813da74f8723ed9b20d
David Wicker <davidwickerhf@gmail.com>
2023-12-07 20:51:44 +0100

    Add download buttons

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
 	import type { PageData } from './$types';
-	import { padding, paddingY } from '$lib/constants/theme';
+	import { padding, paddingY, theme } from '$lib/constants/theme';
 
 	import MenuSectionButton from './MenuSectionButton.svelte';
 	import MenuSection from './MenuSection.svelte';
 	import LandingPicture from '$lib/components/LandingPicture.svelte';
 
 	import { verticalScroll } from '$lib/store/ScrollStore';
+	import Button from '$lib/components/Button.svelte';
 
 	export let data: PageData;
 
@@ -60,25 +61,38 @@
 <!-- Landing picture -->
 <LandingPicture title={$_('contents.menu.title')} />
 
-<div class=" flex-row flex {padding} {paddingY} gap-20 max-h-fit">
-	<!-- Sidebar -->
-	<div class="flex-col hidden max-h-full text-darkbrown-three lg:flex" bind:this={nav}>
-		<!-- <div class="w-full bg-black" style="height: {divHeight}px;" /> -->
-		<!-- Sidebar item -->
-		<dvi class="flex flex-col gap-8 min-w-max">
-			{#each data.sections as section, index}
-				<MenuSectionButton bind:inView bind:selected={selected[index]} {section} />
-			{/each}
-		</dvi>
-	</div>
+<div class=" {padding} {paddingY} flex flex-col gap-20">
+	<div class="flex flex-row gap-20 max-h-fit">
+		<!-- Sidebar -->
+		<div class="flex-col hidden max-h-full text-darkbrown-three lg:flex" bind:this={nav}>
+			<!-- <div class="w-full bg-black" style="height: {divHeight}px;" /> -->
+			<!-- Sidebar item -->
+			<dvi class="flex flex-col gap-8 min-w-max">
+				{#each data.sections as section, index}
+					<MenuSectionButton bind:inView bind:selected={selected[index]} {section} />
+				{/each}
+			</dvi>
+		</div>
 
-	<!-- Menu contents -->
-	<div
-		bind:this={contents}
-		class="flex flex-col gap-10 lg:border-l-[1px] border-darkbrown-three border-opacity-20 w-full"
-	>
-		{#each data.sections as section, index}
-			<MenuSection bind:selected={selected[index]} {section} />
-		{/each}
+		<!-- Menu contents -->
+		<div
+			bind:this={contents}
+			class="flex flex-col gap-10 lg:border-l-[1px] border-darkbrown-three border-opacity-20 w-full"
+		>
+			{#each data.sections as section, index}
+				<MenuSection bind:selected={selected[index]} {section} />
+			{/each}
+		</div>
+	</div>
+	<div class="flex flex-row gap-6">
+		<Button url="/menus/Menukaart.pdf" bgColor={theme.colors.darkbrown.three} textColor={'white'}
+			>{$_('contents.menu.downloadMenu')}</Button
+		>
+		<Button url="/menus/Wijnkaart.pdf" bgColor={theme.colors.darkbrown.three} textColor={'white'}
+			>{$_('contents.menu.downloadWines')}</Button
+		>
+		<Button url="/menus/Drankenkaart.pdf" bgColor={theme.colors.darkbrown.three} textColor={'white'}
+			>{$_('contents.menu.downloadDrinks')}</Button
+		>
 	</div>
 </div>
