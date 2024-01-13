18918b0a68e37baa03b12da5433e335d5407790f
David Wicker <davidwickerhf@gmail.com>
2024-01-13 14:34:44 +0100

    Add Analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,6 +5,11 @@ import type { LayoutLoad } from './$types';
 import type { MenuSection, Dish } from '$lib/constants/types.js';
 import Papa from 'papaparse';
 
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
+
 export const load: LayoutLoad = async ({ fetch, params }) => {
 	await waitLocale();
 	if (browser) {
